ef19d83a43fd36efd50f35422b54db8f2ecc138f
SteveGuoh <hghandle@outlook.com>
2023-10-02 00:52:46 +0800

    added checkout, review and manage loan features

--- src/main/java/com/stevestore/springbootlibrary/controller/BookController.java
+++ src/main/java/com/stevestore/springbootlibrary/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.stevestore.springbootlibrary.controller;
 
 import com.stevestore.springbootlibrary.entity.Book;
+import com.stevestore.springbootlibrary.resoponsemodels.ShelfCurrentLoansResponse;
 import com.stevestore.springbootlibrary.service.BookService;
 import com.stevestore.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -16,6 +19,15 @@ public class BookController {
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @GetMapping("/secure/currentloans")
+    public List<ShelfCurrentLoansResponse> currentLoans(@RequestHeader(value = "Authorization") String token)
+    throws Exception
+    {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.currentLoans(userEmail);
+
     }
 
     @GetMapping("/secure/currentloans/count")
@@ -37,6 +49,20 @@ public class BookController {
         return bookService.checkOutBook(userEmail, bookId);
     }
 
+    @PutMapping("/secure/return")
+    public void returnBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception{
 
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.returnBook(userEmail, bookId);
+
+    }
+
+    @PutMapping("/secure/renew/loan")
+    public void renewLoan(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception{
+
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.renewLoan(userEmail, bookId);
+
+    }
 
 }
